798e32c20dd3e768341c035fe5fe2d079845bedc
ethan509 <ethanisyes@gmail.com>
2022-08-01 00:20:54 +0900

    ch26. Project 단어검색프로그램 만들기 v1.2 채널 추가중(~ing)

--- book-tucker/ch26/ch26.go
+++ book-tucker/ch26/ch26.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 	"sync"
 )
+
+type LineInfo struct {
+	LineNo int
+	Line   string
+}
+
+type FindInfo struct {
+	FileName string
+	lines    *list.List
+}
 
 func readArgs() (string, []string) {
 	allArgs := os.Args[1:]
@@ -22,7 +33,7 @@ func readArgs() (string, []string) {
 	return allArgs[0], allArgs[1:]
 }
 
-func SearchingWord(wg *sync.WaitGroup, findingWord string, fileName string) {
+func SearchingWord(wg *sync.WaitGroup, ch chan FindInfo, findInfos *list.List, findingWord string, fileName string) {
 
 	// File Open & Read
 	file, err := os.Open(fileName) // For read access.
@@ -30,6 +41,12 @@ func SearchingWord(wg *sync.WaitGroup, findingWord string, fileName string) {
 		log.Fatal(err)
 	}
 	defer file.Close()
+
+	var findInfo FindInfo
+	findInfo.FileName = fileName
+
+	lineInfos := list.New()
+	var lineInfo LineInfo
 
 	// Searching word
 	scanner := bufio.NewScanner(file)
@@ -39,11 +56,30 @@ func SearchingWord(wg *sync.WaitGroup, findingWord string, fileName string) {
 		line := scanner.Text()
 		//fmt.Println("Args: ", line)
 		if strings.Contains(line, findingWord) {
-			fmt.Printf("[%s]\t%d\t%s\n", fileName, lineNo, line)
+
+			lineInfo.LineNo = lineNo
+			lineInfo.Line = line
+
+			lineInfos.PushBack(lineInfo)
+
+			fmt.Printf("[%s]\t%d\t%s\n", fileName, lineInfo.LineNo, lineInfo.Line)
 		}
 		lineNo++
 	}
+	findInfo.lines = lineInfos
+
+	ch <- findInfo
+	findInfos.PushBack(findInfo)
+
 	wg.Done()
+}
+
+func Result(ch chan FindInfo) {
+	for findInfo := range ch {
+		for e := findInfo.lines.Front(); e != nil; e = e.Next() {
+			fmt.Printf("[%s]\t%v\n", findInfo.FileName, e.Value)
+		}
+	}
 }
 
 func main() {
@@ -54,10 +90,13 @@ func main() {
 	fmt.Println("Args: ", findingWord, fileNames)
 
 	wg.Add(len(fileNames))
+	ch := make(chan FindInfo)
+	//findInfos := list.New()
 
-	for _, fileName := range fileNames {
-		go SearchingWord(&wg, findingWord, fileName)
-	}
+	Result(ch)
+	// for _, fileName := range fileNames {
+	// 	go SearchingWord(&wg, &ch, findInfos, findingWord, fileName)
+	// }
 	wg.Wait()
 
 }
